1a2d6aabdc2ff20a8834200a7d2dd00f050885b2
mjbarlow <mjbarlow@gmail.com>
2019-03-31 20:24:16 +0100

    Hook up Blogs listings

---
+++ U8StarterKit.Web/Models/ViewModels/BlogsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using U8StarterKit.Web.Models.Umbraco;
+
+namespace U8StarterKit.Web.Models.ViewModels
+{
+    public class BlogsViewModel
+    {
+        public Blog Blog {get; set;}
+        public int ItemsToShow {get; set;}
+        public List<Blogpost> BlogsPosts {get; set;}
+        public bool IsHalfWidth { get; set; }
+        public bool HasPaging { get; set; }
+
+    }
+}
